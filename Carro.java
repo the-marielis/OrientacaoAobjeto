16972e7e6ed430ab954937569874545de6351c90
Marieli Teixeira <142545847+the-marielis@users.noreply.github.com>
2025-03-06 21:18:55 -0300

    Add files via upload

---
+++ Carro.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package associacao;
+
+/**
+ *
+ * @author marie
+ */
+public class Carro {
+    private int ano;
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+    private String modelo;
+    private String placa;
+    private float valor;
+    
+}
